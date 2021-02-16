5792b99c260854a416803cf08256e2c6d9c464de
jh k <rlawpgud@naver.com>
2021-02-16 21:23:40 +0900

    finish base backend

---
+++ client.js
@@ -0,0 +1,6 @@
+import { PrismaClient } from "@prisma/client";
+
+const client = new PrismaClient() // client 객체 생성
+
+export default client;
+
--- server.js
+++ server.js
@@ -1,52 +1,13 @@
-import { PrismaClient } from "@prisma/client";
-import { ApolloServer, ExpandAbstractTypes, gql } from "apollo-server";
-
-const client = new PrismaClient() // client 객체 생성 
-
-const typeDefs = gql`
-  type Movie {
-    id: Int!
-    title: String!
-    year: Int!
-    genre: String
-    createdAt: String!
-    updatedAt: String!
-  }
-  type Query {
-    movies: [Movie]
-    movie(id:Int!): Movie
-  }
-  type Mutation {
-    createMovie(title: String!, year: Int!, genre: String): Movie
-    deleteMovie(id:Int!): Movie
-    updateMovie(id: Int!, year: Int!): Movie
-  }
-`;
-
-const resolvers = {
-  Query: {
-    movies: () => client.movie.findMany(), //여기서 movie는 prisma에서 정의한 schema (model) 테이블 값이다.
-    movie: (_, {id}) => client.movie.findUnique({where:{id}})
-
-  },
-  Mutation: { //movie 테이블 값에 데이터를 추가해보자
-    createMovie: (_, { title, year, genre }) => client.movie.create({
-      data: {
-      title,
-      year,
-      genre,
-    }}),
-    deleteMovie: (_, { id }) => client.movie.delete({where: { id }}),
-    updateMovie: (_, { id, year }) => client.movie.update({where: { id }, data: { year }}),
-  },
-}
-
+require("dotenv").config();
+import { ApolloServer } from "apollo-server";
+import schema from "./schema"
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+  schema,
 });
+
+const PORT = process.env.PORT;
 
 server
   .listen()
-  .then(() => console.log("Server is running http://localhost:4000/"));
+  .then(() => console.log(`🚀 Server is running http://localhost:${PORT} ✅`));
