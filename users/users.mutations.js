5c622ca14bd36d968147d5f078b166df3803ce56
jh k <rlawpgud@naver.com>
2021-02-19 21:14:57 +0900

    createUser.1.0

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -1,3 +1,4 @@
+import bcrypt from "bcrypt"
 import client from "../client";
 
 export default {
@@ -20,7 +21,17 @@ export default {
                         }
                     ],
                 },
-            });
+            }); // db에 userName와 email가 이미 있는지 확인하는 로직
+            const uglyPassword = await bcrypt.hash(password, 10);
+            return client.user.create({
+                data: {
+                    userName,
+                    email,
+                    firstName,
+                    lastName,
+                    password: uglyPassword,
+                }
+            })
         }
     }
 };
