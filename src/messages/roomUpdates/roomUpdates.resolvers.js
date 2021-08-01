63204b4a1f12fa00204142530d760f65febbc3b7
jh k <rlawpgud86@naver.com>
2021-08-01 19:07:06 +0900

    rerestart

--- src/messages/roomUpdates/roomUpdates.resolvers.js
+++ src/messages/roomUpdates/roomUpdates.resolvers.js
@@ -1,6 +1,6 @@
 import { withFilter } from "apollo-server";
 import client from "../../client";
-import { NEW_MESSAGE } from "../../../constants";
+import { NEW_MESSAGE } from "../../constants";
 import pubsub from "../../../pubsub";
 
 export default {
